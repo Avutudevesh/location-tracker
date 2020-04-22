e670bfd660445a9170869d0ae013a4e4065300f9
Devesh Avutu <devesh14may@gmail.com>
2020-04-22 17:02:17 +0530

    created useLocation hook

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,20 +1,14 @@
 import React, { useContext } from "react";
 import { Text } from "react-native-elements";
 import { View, StyleSheet, ActivityIndicator } from "react-native";
-import MapView, { Polyline } from "react-native-maps";
+import MapView, { Polyline, Circle } from "react-native-maps";
 import { Context as LocationContext } from "../context/LocationContext";
 
 const Map = () => {
 	const {
 		state: { currentLocation },
 	} = useContext(LocationContext);
-	// let points = [];
-	// for (let i = 0; i < 20; i++) {
-	// 	points.push({
-	// 		latitude: 37.33233 + i * 0.001,
-	// 		longitude: -122.0312 + i * 0.001,
-	// 	});
-	// }
+
 	if (!currentLocation) {
 		return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
 	}
@@ -33,6 +27,12 @@ const Map = () => {
 					longitudeDelta: 0.01,
 				}}
 			>
+				<Circle
+					center={currentLocation.coords}
+					radius={30}
+					strokeColor="rgba(158,158,255,1.0)"
+					fillColor="rgba(158,158,255,0.3)"
+				/>
 				{/* <Polyline coordinates={points} /> */}
 			</MapView>
 		</>
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,48 +1,26 @@
-// import "../_mockLocation";
-import React, { useEffect, useState, useContext } from "react";
+import "../_mockLocation";
+import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
-import { SafeAreaView } from "react-navigation";
+import { SafeAreaView, withNavigationFocus } from "react-navigation";
 import { Text } from "react-native-elements";
-import {
-	requestPermissionsAsync,
-	watchPositionAsync,
-	Accuracy,
-} from "expo-location";
 import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
-const TrackCreateScreen = () => {
-	const [err, setErr] = useState(null);
+import useLocation from "../hooks/useLocation";
+import TrackForm from "../components/TrackForm";
+
+const TrackCreateScreen = ({ isFocused }) => {
 	const { addLocation } = useContext(LocationContext);
-	const startWatching = async () => {
-		try {
-			await requestPermissionsAsync();
-			await watchPositionAsync(
-				{
-					accuracy: Accuracy.BestForNavigation,
-					timeInterval: 1000,
-					distanceInterval: 10,
-				},
-				(location) => {
-					addLocation(location);
-				}
-			);
-		} catch (e) {
-			console.log(e);
-			setErr(e);
-		}
-	};
-	useEffect(() => {
-		startWatching();
-	}, []);
+	const [err] = useLocation(isFocused, addLocation);
 	return (
 		<SafeAreaView forceInset={{ top: "always" }}>
 			<Text h2>Create a Track</Text>
 			<Map />
 			{err ? <Text>Enable Location Services to start tracking</Text> : null}
+			<TrackForm />
 		</SafeAreaView>
 	);
 };
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
